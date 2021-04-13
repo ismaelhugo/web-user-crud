b7b14530c5def931388d24365429da36c1f543f3
irizzo <isabelle965rizzo@gmail.com>
2021-04-13 20:55:21 -0300

    conexão ok amém

--- back-end/index.js
+++ back-end/index.js
@@ -1,31 +1,26 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
+const express = require('express')
+const app = express()
+const port = 3000
 const bodyParser = require('body-parser');
-const mysql = require('mysql')
+const mysql = require('mysql');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json);
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "BDe:1RcS:uMG",
-  database: 'web1'
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "BDe123",
+    database: "web1"
+  });
 
   con.query('select * from usuario', function(err, result){
     res.send(result)
-    console.log(err)
+    
+    if (err) console.log(err);
   })
 })
 
